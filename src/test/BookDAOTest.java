c7055c99a287aed88d1e8a2321aacac05fbd0e15
bjohnson1986 <bjohnson@hotmail.com.au>
2015-09-27 20:34:12 +1000

    Finished BookDAOTest.java - just needs commenting.

--- src/test/BookDAOTest.java
+++ src/test/BookDAOTest.java
@@ -1,8 +1,5 @@
 package test;
 
-
-
-
 import library.daos.BookHelper;
 import library.daos.BookDAO;
 import library.entities.Book;
@@ -10,8 +7,8 @@ import library.interfaces.entities.IBook;
 import static org.junit.Assert.*;
 
 import org.mockito.Mockito;
-
 import static org.mockito.Mockito.when;
+
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.contains;
 
@@ -22,13 +19,11 @@ import org.junit.Test;
 public class BookDAOTest {
 	
 	private BookHelper bookHelper_ = Mockito.mock(BookHelper.class);
-	@SuppressWarnings("unused")
-	private BookDAO bookDao_= new BookDAO(bookHelper_);
 	private int bookId_ = 1;
-	
 
 	@Before
-	public void setUp() throws Exception {	
+	public void setUp() throws Exception {
+		//Simulate the BookHelper's makrBook method.
 		when(bookHelper_.makeBook(contains("Author0"), contains("BookTitle0"), contains("BookCallNumber0"), anyInt())).thenReturn(new Book("Author0", "BookTitle0", "BookCallNumber0", bookId_++));
 		when(bookHelper_.makeBook(contains("Author1"), contains("BookTitle1"), contains("BookCallNumber1"), anyInt())).thenReturn(new Book("Author1", "BookTitle1", "BookCallNumber1", bookId_++));
 		when(bookHelper_.makeBook(contains("Author0"), contains("BookTitle2"), contains("BookCallNumber2"), anyInt())).thenReturn(new Book("Author0", "BookTitle2", "BookCallNumber2", bookId_++));
@@ -53,72 +48,84 @@ public class BookDAOTest {
 	
 	@Test
 	public final void testAddBook() {
-		BookDAO bookDao_= new BookDAO(bookHelper_);
-		bookDao_.addBook("Author0", "BookTitle0", "BookCallNumber0");
-		bookDao_.addBook("Author1", "BookTitle1", "BookCallNumber1");
-		bookDao_.addBook("Author0", "BookTitle2", "BookCallNumber2");
-		assertEquals(3, bookDao_.listBooks().size());
+		//Setup new BookDao
+		BookDAO bookDao= new BookDAO(bookHelper_);
+		//Add three books
+		bookDao.addBook("Author0", "BookTitle0", "BookCallNumber0");
+		bookDao.addBook("Author1", "BookTitle1", "BookCallNumber1");
+		bookDao.addBook("Author0", "BookTitle2", "BookCallNumber2");
+		//Verify the books exist.
+		assertEquals(3, bookDao.listBooks().size());
 	}
 
 	@Test
 	public final void testGetBookByID() {
-		System.out.println(bookId_);
-		System.out.println(bookId_);
-		BookDAO bookDao_= new BookDAO(bookHelper_);
-		bookDao_.addBook("Author0", "BookTitle0", "BookCallNumber0");
-		bookDao_.addBook("Author1", "BookTitle1", "BookCallNumber1");
-		bookDao_.addBook("Author0", "BookTitle2", "BookCallNumber2");
-		IBook book1 = bookDao_.getBookByID(1);
-		IBook book2 = bookDao_.getBookByID(2);
-		IBook book3 = bookDao_.getBookByID(3);
-		System.out.println(book1.getAuthor());
+		//Setup new BookDao
+		BookDAO bookDao= new BookDAO(bookHelper_);
+		//Add three books
+		bookDao.addBook("Author0", "BookTitle0", "BookCallNumber0");
+		bookDao.addBook("Author1", "BookTitle1", "BookCallNumber1");
+		bookDao.addBook("Author0", "BookTitle2", "BookCallNumber2");
+		//Get all three books by their Id
+		IBook book1 = bookDao.getBookByID(1);
+		IBook book2 = bookDao.getBookByID(2);
+		IBook book3 = bookDao.getBookByID(3);
+		//Check that each book retrieved by their Id has correct description
 		assertTrue((book1.getAuthor() == "Author0") && (book1.getTitle() == "BookTitle0") && (book1.getCallNumber() == "BookCallNumber0"));
 		assertTrue((book2.getAuthor() == "Author1") && (book2.getTitle() == "BookTitle1") && (book2.getCallNumber() == "BookCallNumber1"));
 		assertTrue((book3.getAuthor() == "Author0") && (book3.getTitle() == "BookTitle2") && (book3.getCallNumber() == "BookCallNumber2"));
 	}
 
 	@Test
-	public final void testListBooks() {		
-		bookId_ = 1;
+	public final void testListBooks() {
+		//Setup new BookDao
 		BookDAO bookDao =  new BookDAO(bookHelper_);
+		//Add two books
 		bookDao.addBook("Author0", "BookTitle0", "BookCallNumber0");
 		bookDao.addBook("Author1", "BookTitle1", "BookCallNumber1");
-		assertEquals(bookDao.listBooks().size(), 2);
+		//Check that both books are listed
+		assertEquals(2, bookDao.listBooks().size());
 	}
 
 	@Test
 	public final void testFindBooksByAuthor() {
-		bookId_ = 20;
-		BookDAO bookDao_= new BookDAO(bookHelper_);
-		bookDao_.addBook("Author0", "BookTitle0", "BookCallNumber0");
-		bookDao_.addBook("Author1", "BookTitle1", "BookCallNumber1");
-		bookDao_.addBook("Author0", "BookTitle2", "BookCallNumber2");
-		assertEquals(bookDao_.findBooksByAuthor("Author0").size(), 2);
-		assertEquals(bookDao_.findBooksByAuthor("Author1").size(), 1);		
+		//Setup new BookDao
+		BookDAO bookDao= new BookDAO(bookHelper_);
+		//Add three books
+		bookDao.addBook("Author0", "BookTitle0", "BookCallNumber0");
+		bookDao.addBook("Author1", "BookTitle1", "BookCallNumber1");
+		bookDao.addBook("Author0", "BookTitle2", "BookCallNumber2");
+		//Check that books are retrievable by author	
+		assertEquals(2, bookDao.findBooksByAuthor("Author0").size());
+		assertEquals(1, bookDao.findBooksByAuthor("Author1").size());		
 	}
 
 	@Test
 	public final void testFindBooksByTitle() {
-		bookId_ = 30;
-		BookDAO bookDao_= new BookDAO(bookHelper_);
-		bookDao_.addBook("Author0", "BookTitle0", "BookCallNumber0");
-		bookDao_.addBook("Author1", "BookTitle1", "BookCallNumber1");
-		bookDao_.addBook("Author0", "BookTitle2", "BookCallNumber2");
-		assertEquals(bookDao_.findBooksByTitle("BookTitle0").size(), 1);
-		assertEquals(bookDao_.findBooksByTitle("BookTitle1").size(), 1);
-		assertEquals(bookDao_.findBooksByTitle("BookTitle2").size(), 1);
+		//Setup new BookDao
+		BookDAO bookDao= new BookDAO(bookHelper_);
+		//Add three books
+		bookDao.addBook("Author0", "BookTitle0", "BookCallNumber0");
+		bookDao.addBook("Author1", "BookTitle1", "BookCallNumber1");
+		bookDao.addBook("Author0", "BookTitle2", "BookCallNumber2");
+		//Check that books are retrievable by title	
+		assertEquals(bookDao.findBooksByTitle("BookTitle0").size(), 1);
+		assertEquals(bookDao.findBooksByTitle("BookTitle1").size(), 1);
+		assertEquals(bookDao.findBooksByTitle("BookTitle2").size(), 1);
 	}
 
 	@Test
 	public final void testFindBooksByAuthorTitle() {
-		bookId_ = 40;
-		BookDAO bookDao_= new BookDAO(bookHelper_);
-		bookDao_.addBook("Author0", "BookTitle0", "BookCallNumber0");
-		bookDao_.addBook("Author1", "BookTitle1", "BookCallNumber1");
-		bookDao_.addBook("Author0", "BookTitle2", "BookCallNumber2");
-		assertEquals(bookDao_.findBooksByAuthorTitle("Author0", "BookTitle0").size(), 1);
-		assertEquals(bookDao_.findBooksByAuthorTitle("Author1", "BookTitle1").size(), 1);
-		assertEquals(bookDao_.findBooksByAuthorTitle("Author0", "BookTitle2").size(), 1);
+		//Setup new BookDao
+		BookDAO bookDao= new BookDAO(bookHelper_);
+		//Add three books
+		bookDao.addBook("Author0", "BookTitle0", "BookCallNumber0");
+		bookDao.addBook("Author1", "BookTitle1", "BookCallNumber1");
+		bookDao.addBook("Author0", "BookTitle2", "BookCallNumber2");
+		//Check that books are retrievable by author and title	
+		assertEquals(bookDao.findBooksByAuthorTitle("Author0", "BookTitle0").size(), 1);
+		assertEquals(bookDao.findBooksByAuthorTitle("Author1", "BookTitle1").size(), 1);
+		assertEquals(bookDao.findBooksByAuthorTitle("Author0", "BookTitle2").size(), 1);
 	}
 
 }
